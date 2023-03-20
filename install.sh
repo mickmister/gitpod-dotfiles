c66991179f973cbac63c59ae88584354139a2780
Michael Kochell <mjkochell@gmail.com>
2023-03-20 21:30:40 +0000

    add global gitignore

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ cd /workspace
 git clone https://github.com/mickmister/gitpod-dotfiles.git
 
 brew install gh
+
+globalGitIgnore="/workspace/.global_gitignore"
+touch $globalGitIgnore
+echo "_debug" >> $globalGitIgnore
+git config --global core.excludesfile $globalGitIgnore
